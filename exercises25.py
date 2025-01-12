d228d849a0eb01081ae81fbecd29abdeb20f72ec
fazza-tech <ebranfazza@gmail.com>
2025-01-13 01:19:36 +0530

    const var added

--- exercises25.py
+++ exercises25.py
@@ -9,3 +9,17 @@ print(msg)
 filename = "python_notes.txt"
 filename = filename.removesuffix(".txt")
 print(filename)
+print(2.03+0.025)
+
+print(4/2)
+print(2+1.0)
+
+one_crore = 1_000_00_000
+print(one_crore)
+
+x,myName,y = 2,"fasal",6
+print(x,myName,y)
+
+AGE = 23
+print(AGE)
+AGE = 12
