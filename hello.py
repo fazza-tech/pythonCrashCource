45b8a79971c0baf26cab4e7a88998258996f86bc
fazza-tech <ebranfazza@gmail.com>
2025-01-13 00:50:13 +0530

    hellop.py

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+hello = "Hello"
+world = "world"
+
+hello_world = f"{hello} {world}"
+hello_world = hello_world.upper()
+print(hello_world)
+
+name = "Fasal "
+name.strip()
+print(name)
+
+brototype_url = "http://www.brototype.com"
+brototype_url = brototype_url.removeprefix("http://")
+print(brototype_url)
+
+ 
