dc3ec246576947cc9ac69a96d7bba1c9d3e81494
fazza-tech <ebranfazza@gmail.com>
2025-01-13 00:50:44 +0530

    exercises

---
+++ exercises25.py
@@ -0,0 +1,11 @@
+#2-3. Personal Message: Use a variable to represent a person’s name, and print
+#a message to that person. Your message should be simple, such as, “Hello Eric,
+#would you like to learn some Python today?”
+
+name = "Eric"
+msg = f"Hello {name},would you like to learn some Python today?"
+print(msg)
+
+filename = "python_notes.txt"
+filename = filename.removesuffix(".txt")
+print(filename)
