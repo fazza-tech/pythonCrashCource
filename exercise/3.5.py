30da93c8659e16e9a74aa275699259d65b5091d9
fazza-tech <ebranfazza@gmail.com>
2025-01-14 11:11:34 +0530

    exercise 3.5

---
+++ exercise/3.5.py
@@ -0,0 +1,29 @@
+
+members = ['akshara','vrindha','stelna']
+print(f"{members[0]} can't cum anymore guyzzzz.")
+del members[0]
+members.insert(0,"Teena")
+print(members)
+
+print(f"Hey {members[0]},{members[1]},{members[2]}.... I can't wait for u anymore guyzz... Please come and eat my cake💘")
+
+print(f"hey guyz..i found that we got a big pool here...im going to add more...")
+#Use insert() to add one new guest to the beginning of your list.
+members.insert(0,'Mia ')
+print(members)
+
+#Use insert() to add one new guest to the middle of your list.
+members.insert(2,'Gabbie ')
+print(members)
+
+#Use append() to add one new guest to the end of your list.
+members.append(' Ames')
+print(members)
+
+#Print a new set of invitation messages, one for each person in your list.
+print(f"Hi {members[0]}, Please come to my Party🎉💃")
+print(f"Hi {members[1]}, Please come to my Party🎉💃")
+print(f"Hi {members[2]}, Please come to my Party🎉💃")
+print(f"Hi {members[3]}, Please come to my Party🎉💃")
+print(f"Hi {members[4]}, Please come to my Party🎉💃")
+print(f"Hi {members[5]}, Please come to my Party🎉💃")
