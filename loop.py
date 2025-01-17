4da9a075b57056d7852c6929e10cc7ceb9b2472b
fazza-tech <ebranfazza@gmail.com>
2025-01-17 23:49:33 +0530

    indentation errors::

--- loop.py
+++ loop.py
@@ -2,4 +2,11 @@ magicians = ['tory','alana','abella','dani'] #created list
 
 #creating loop 
 for magician in magicians:
-    print(magician)
+    print(f"Its an amazing bang {magician.title()}")
+    print(f"I cant wait for your next perfomance {magician.title()} \n")
+
+list_of_items = ['tomato','carrot','banana']
+
+print("Your cart:")
+for lists in list_of_items:
+    print(f"\t {lists}") 
