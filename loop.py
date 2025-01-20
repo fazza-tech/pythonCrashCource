9bb43a5c0e7e58049b1eac51a4b18fde907a1de8
fazza-tech <ebranfazza@gmail.com>
2025-01-21 01:21:38 +0530

    range() func

--- loop.py
+++ loop.py
@@ -10,3 +10,27 @@ list_of_items = ['tomato','carrot','banana']
 print("Your cart:")
 for lists in list_of_items:
     print(f"\t {lists}") 
+
+print("my fvrt pizzas:")
+
+pizzas = ["angela","mia malkova","ablea"]
+
+for piza in pizzas:
+    print(f"my fvrt pizza is {piza}")
+
+for value in range(1,11):
+    print(value)
+
+numbers = list(range(1,7))
+print(numbers)
+
+skip_nums=list(range(0,11,2))
+print(skip_nums)
+
+squares = []
+
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
